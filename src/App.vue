bb277264c0a3691212e37f02e3a80f9cf8fde8e7
Gammelinne <kylian.renault@viacesi.fr>
2023-10-03 13:48:54 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    {{ $t('message.hello') }}
+  </div>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import i18n from './i18n';
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(i18n)
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
